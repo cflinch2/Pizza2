d6891df82bb704d6130be5b2299cf4f16589227c
Chris Flinchbaugh <cflinch2@ycp.edu>
2013-03-18 11:55:17 -0400

    create PizzaView

---
+++ Pizza/src/edu/ycp/cs320/pizza/client/PizzaView.java
@@ -0,0 +1,16 @@
+package edu.ycp.cs320.pizza.client;
+
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Label;
+
+public class PizzaView extends Composite {
+	public PizzaView() {
+		
+		
+		
+		Label Size = new Label("Size");
+		initWidget(Size);
+		
+	}
+
+}
